f055bd163b34303232a25af05cc5da949d643763
Kaspok <kaspok251198uU@yandex.ru>
2021-08-11 21:44:14 +0300

    Добавил атачменты (скриншот, видео, элементы страрницы, логи с браузера), базовый действия до и после тестов TestBase для тестов

---
+++ src/test/java/com/bercut/tests/TestBase.java
@@ -0,0 +1,28 @@
+package com.bercut.tests;
+
+import com.bercut.helpers.Attach;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
